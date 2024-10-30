7aa5d6e8a49044010761138dbdb0c9f74f15b873
EverlastingHolder <47381469+EverlastingHolder@users.noreply.github.com>
2024-10-30 16:24:58 +0700

    Update Utils.swift

--- MaLiang/Classes/Math & Utils/Utils.swift
+++ MaLiang/Classes/Math & Utils/Utils.swift
@@ -36,7 +36,7 @@ extension MTLTexture {
             origin: MTLOrigin(x: 0, y: 0, z: 0),
             size: MTLSize(width: width, height: height, depth: 1)
         )
-        let bytesPerRow = 4 * width
+        let bytesPerRow = 4 * 5160
         let data = Data(capacity: Int(bytesPerRow * height))
         if let bytes = data.withUnsafeBytes({ $0.baseAddress }) {
             replace(region: region, mipmapLevel: 0, withBytes: bytes, bytesPerRow: bytesPerRow)
