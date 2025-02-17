7cc1542497b9e21f1e94b4f40f1ba590a54d00d2
roman.moshkovcev <roman.moshkovcev@myworkflow.ru>
2025-02-17 11:30:02 +0700

    feat: add maskBrush

---
+++ MaLiang/Classes/Brush/MaskBrush.swift
@@ -0,0 +1,76 @@
+//
+//  MaskBrush.swift
+//  MaLiang
+//
+//  Created by roman.moshkovcev on 17.02.2025.
+//  Copyright © 2025 MoveApp. All rights reserved.
+//
+
+import Foundation
+import MetalKit
+
+open class MaskBrush: Brush {
+    
+    public private(set) var maskTexture: MTLTexture?
+    
+    required public init(maskTexture: MTLTexture?, name: String?, textureID: String?, target: MLCanvas) {
+        self.maskTexture = maskTexture
+        super.init(name: name, textureID: textureID, target: target)
+    }
+    
+    required public init(name: String?, textureID: String?, target: MLCanvas) {
+        fatalError("init(name:textureID:target:) has not been implemented")
+    }
+    
+    /// Переопределение метода шейдеров для кисти
+    open override func makeShaderFragmentFunction(from library: MTLLibrary) -> MTLFunction? {
+        // Используем пользовательский фрагментный шейдер для растушевки
+        return library.makeFunction(name: "fragment_mask_func")
+    }
+    
+    /// Настройка параметров смешивания для эффекта растушевки
+    open override func setupBlendOptions(for attachment: MTLRenderPipelineColorAttachmentDescriptor) {
+        attachment.isBlendingEnabled = true
+        
+        attachment.rgbBlendOperation = .add
+        attachment.alphaBlendOperation = .add
+        
+        attachment.sourceRGBBlendFactor = .sourceAlpha
+        attachment.sourceAlphaBlendFactor = .sourceAlpha
+        
+        attachment.destinationRGBBlendFactor = .oneMinusSourceAlpha
+        attachment.destinationAlphaBlendFactor = .oneMinusSourceAlpha
+    }
+    
+    open override func render(lineStrip: LineStrip, on renderTarget: RenderTarget? = nil) {
+        
+        let renderTarget = renderTarget ?? target?.screenTarget
+        
+        guard !lineStrip.lines.isEmpty, let target = renderTarget else {
+            return
+        }
+        
+        /// make sure reusable command buffer is ready
+        target.prepareForDraw()
+        
+        /// get commandEncoder form resuable command buffer
+        let commandEncoder = target.makeCommandEncoder()
+        
+        commandEncoder?.setRenderPipelineState(pipelineState)
+        
+        if let vertexBuffer = lineStrip.retrieveBuffers(rotation: rotation) {
+            commandEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+            commandEncoder?.setVertexBuffer(target.uniformBuffer, offset: 0, index: 1)
+            commandEncoder?.setVertexBuffer(target.transformBuffer, offset: 0, index: 2)
+            if let texture = texture {
+                commandEncoder?.setFragmentTexture(texture, index: 0)
+            }
+            if let texture = maskTexture {
+                commandEncoder?.setFragmentTexture(texture, index: 1)
+            }
+            commandEncoder?.drawPrimitives(type: .point, vertexStart: 0, vertexCount: lineStrip.vertexCount)
+        }
+        
+        commandEncoder?.endEncoding()
+    }
+}
--- MaLiang/Classes/MetalBase/Shaders.metal
+++ MaLiang/Classes/MetalBase/Shaders.metal
@@ -201,3 +201,14 @@ fragment float4 fragment_point_func_without_texture(Point point_data [[ stage_in
     }
     return point_data.color;
 }
+
+fragment float4 fragment_mask_func(Point point_data [[ stage_in ]],
+                                   texture2d<float> tex2d [[ texture(0) ]],
+                                   texture2d<float> mask [[ texture(1) ]],
+                                   float2 pointCoord [[point_coord]]
+                                   )
+{
+    constexpr sampler textureSampler(mag_filter::linear, min_filter::linear);
+    float4 color = float4(tex2d.sample(textureSampler, pointCoord));
+    return float4(color.rgb, color.a * point_data.color.a);
+}
